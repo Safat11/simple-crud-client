3e79d7be3b5a596f8b47829848b85f25aa90d26e
Safat11 <safat3622@gmail.com>
2025-06-17 13:26:04 +0600

    (Recap) Create Read And Delete And Show Remaining Users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
-    const users = useLoaderData()
+    const loadedUsers = useLoaderData()
+    const [users, setUsers] = useState(loadedUsers);
 
     const handleDelete = _id => {
         console.log('delete', _id);
-        
+
         fetch(`http://localhost:5000/users/${_id}`, {
             method: 'DELETE'
         })
@@ -15,6 +17,8 @@ const Users = () => {
                 console.log(data);
                 if (data.deletedCount > 0) {
                     alert('deleted successfully');
+                    const remaining = users.filter(user=> user._id !== _id);
+                    setUsers(remaining);
                 }
             })
     }
