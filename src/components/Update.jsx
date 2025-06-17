3ded0dfab8861bd259b5f85830c8f016cd63c0a7
Safat11 <safat3622@gmail.com>
2025-06-17 14:45:57 +0600

    Explore Put And Patch And Update User To Database

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -9,6 +9,20 @@ const Update = () => {
         const name = form.name.value;
         const email = form.email.value;
         console.log(name, email)
+
+        const updatedUser = {name, email};
+
+        fetch(`http://localhost:5000/users/${loadedUsers._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
     }
     return (
         <div>
